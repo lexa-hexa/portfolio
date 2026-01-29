77653390cc8c3b337f6ef6dca87df4730ca9f1ef
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-01-29 12:17:25 +0300

    Add reusable header with dynamic base path handling

--- frontend/assets/js/modules/partials.js
+++ frontend/assets/js/modules/partials.js
@@ -1,12 +1,21 @@
 export function loadPartials(){
-    fetch('./partials/header.html')
+    const basePath = window.location.pathname.includes('/pages/')
+    ? '../'
+    : './';
+
+    fetch(`${basePath}partials/header.html`)
     .then(res => res.text())
     .then(html => {
         const header = document.getElementById('site-header');
-        if(header) header.innerHTML = html;
+        if(!header)return; 
+        
+        html = html.replaceAll('__BASE__', basePath);
 
+        header.innerHTML = html;
+        setActiveNav();
         initHamburger();
-    });
+    })
+    .catch(err => console.error('Header load error:', err));
 }
 
 function initHamburger(){
@@ -27,4 +36,36 @@ function initHamburger(){
         btn.setAttribute("aria-expanded", "false");
         }
     });
+}
+
+function setActiveNav(){
+    const links = document.querySelectorAll('.main-navbar a');
+    if(!links.length) return;
+
+    //Bulunan sayfanın adı
+    const currentPath = window.location.pathname;
+    const currentPage = currentPath.split('/').pop() || 'index.html';
+
+    let matched = false;
+
+    links.forEach(link => {
+        const href = link.getAttribute('href');
+
+        if(!href) return;
+
+        if(href.endsWith(currentPage)){
+            link.classList.add('active');
+            matched = true;
+        }else{
+            link.classList.remove('active');
+        }
+    });
+    
+    if(!matched) {
+        links.forEach(link => {
+            if(link.getAttribute('href').includes('index.html')){
+                link.classList.add('active');
+            }
+        });
+    }
 }
